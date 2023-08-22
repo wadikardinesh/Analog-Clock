df4d35a3cdfb7d24d09475a260a868f113c51346
Wadikar Dinesh <dineshwadikar2003@gmail.com>
2023-08-22 09:48:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+setInterval(()=>{
+    let audio = new Audio('sound.mp3');
+    audio.play();
+    
+    let d = new Date();
+    hours = d.getHours();
+    minutes = d.getMinutes();
+    seconds = d.getSeconds();
+
+    hrotation = 30*hours + minutes/2;
+    mrotation = 6*minutes;
+    srotation = 6*seconds;
+    
+    hour.style.transform = `rotate(${hrotation}deg)`
+    minute.style.transform = `rotate(${mrotation}deg)`
+    second.style.transform = `rotate(${srotation}deg)`
+},1000)
